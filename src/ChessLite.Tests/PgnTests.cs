a08221c1b261407c62de847719e4aa45b7764421
Kyle Hickman <kyle@txhickman.com>
2026-01-29 14:22:17 -0600

    feat: add PGN writing

--- src/ChessLite.Tests/PgnTests.cs
+++ src/ChessLite.Tests/PgnTests.cs
@@ -66,4 +66,142 @@ public class PgnTests
 
         await Assert.That(exception).IsNotNull();
     }
+
+    [Test]
+    public async Task Write_Mainline_FormatsMovetextWithResult()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e2e4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e7e5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g1f3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "b8c6"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. e4 e5 2. Nf3 Nc6 *");
+    }
+
+    [Test]
+    public async Task Write_Checkmate_AppendsHashAndResult()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "f2f3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e7e5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g2g4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "d8h4"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. f3 e5 2. g4 Qh4# 0-1");
+    }
+
+    [Test]
+    public async Task Write_Castling_UsesCastleNotation()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g1f3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g8f6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g2g3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g7g6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "f1g2"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "f8g7"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e1g1"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. Nf3 Nf6 2. g3 g6 3. Bg2 Bg7 4. O-O *");
+    }
+
+    [Test]
+    public async Task Write_EnPassant_UsesCaptureNotation()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e2e4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a7a6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e4e5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "d7d5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e5d6"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. e4 a6 2. e5 d5 3. exd6 *");
+    }
+
+    [Test]
+    public async Task Write_Promotion_UsesPromotionSuffix()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a2a4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "h7h5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a4a5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "h5h4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a5a6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "h4h3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a6b7"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "h3g2"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "b7a8q"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. a4 h5 2. a5 h4 3. a6 h3 4. axb7 hxg2 5. bxa8=Q *");
+    }
+
+    [Test]
+    public async Task Write_Check_UsesPlusSuffix()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e2e4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e7e5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "d1h5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "b8c6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "h5f7"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. e4 e5 2. Qh5 Nc6 3. Qxf7+ *");
+    }
+
+    [Test]
+    public async Task Write_DisambiguatesSameTargetRookMove()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e2e4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e7e5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g1f3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g8f6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "f1c4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "f8c5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e1g1"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "e8g8"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "b1c3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a7a6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "d2d3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a6a5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "c1d2"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a5a4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "d1e2"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a4a3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a1e1"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. e4 e5 2. Nf3 Nf6 3. Bc4 Bc5 4. O-O O-O 5. Nc3 a6 6. d3 a5 7. Bd2 a4 8. Qe2 a3 9. Rae1 *");
+    }
+
+    [Test]
+    public async Task Write_DisambiguatesSameTargetKnightMoveByRank()
+    {
+        var game = new Game();
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "g1f3"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a7a6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "f3d4"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "a6a5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "d4b5"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "h7h6"));
+        game.MakeMove(Helpers.MoveFromUci(game.Position, "b1c3"));
+
+        var pgn = Pgn.Write(game);
+
+        await Assert.That(pgn).IsEqualTo("1. Nf3 a6 2. Nd4 a5 3. Nb5 h6 4. N1c3 *");
+    }
 }
---
+++ src/ChessLite/Parsing/PgnWriter.cs
@@ -0,0 +1,222 @@
+using System.Text;
+using ChessLite.Movement;
+using ChessLite.Primitives;
+using ChessLite.State;
+
+namespace ChessLite.Parsing;
+
+internal static class PgnWriter
+{
+    internal static string Write(Game game)
+    {
+        ArgumentNullException.ThrowIfNull(game);
+
+        var builder = new StringBuilder();
+        var position = new Position();
+        var executor = new MoveExecutor();
+
+        var moveIndex = 0;
+        foreach (var move in game.GetMoveHistory())
+        {
+            if (moveIndex > 0)
+            {
+                builder.Append(' ');
+            }
+
+            if (position.WhiteToMove)
+            {
+                builder.Append(position.FullmoveNumber);
+                builder.Append(". ");
+            }
+
+            builder.Append(WriteSanMove(position, move, executor));
+            executor.MakeMove(position, move);
+            moveIndex++;
+        }
+
+        if (moveIndex > 0)
+        {
+            builder.Append(' ');
+        }
+
+        builder.Append(WriteResult(game));
+        return builder.ToString();
+    }
+
+    private static string WriteResult(Game game)
+    {
+        var state = game.GetState();
+        if (state == GameState.Ongoing)
+        {
+            return "*";
+        }
+
+        if (state.IsDraw())
+        {
+            return "1/2-1/2";
+        }
+
+        return game.Position.WhiteToMove ? "0-1" : "1-0";
+    }
+
+    private static string WriteSanMove(Position position, Move move, MoveExecutor executor)
+    {
+        if (move.SpecialMoveType == SpecialMoveType.ShortCastle)
+        {
+            return AppendCheckSuffix(position, move, "O-O", executor);
+        }
+
+        if (move.SpecialMoveType == SpecialMoveType.LongCastle)
+        {
+            return AppendCheckSuffix(position, move, "O-O-O", executor);
+        }
+
+        Span<Move> moves = stackalloc Move[218];
+        var moveCount = MoveGeneration.GenerateLegalMoves(position, moves);
+
+        var pieceChar = GetPieceLetter(move.PieceType);
+        var requiresFile = false;
+        var requiresRank = false;
+
+        if (pieceChar != '\0')
+        {
+            var ambiguousCount = 0;
+            var fromFile = GetFile(move.From);
+            var fromRank = GetRank(move.From);
+            for (var i = 0; i < moveCount; i++)
+            {
+                var candidate = moves[i];
+                if (candidate == move)
+                {
+                    continue;
+                }
+
+                if (candidate.PieceType != move.PieceType || candidate.To != move.To)
+                {
+                    continue;
+                }
+
+                if (candidate.PromotedPieceType != move.PromotedPieceType)
+                {
+                    continue;
+                }
+
+                if (candidate.IsCapture != move.IsCapture)
+                {
+                    continue;
+                }
+
+                ambiguousCount++;
+                if (GetFile(candidate.From) != fromFile)
+                {
+                    requiresFile = true;
+                }
+                else
+                {
+                    requiresRank = true;
+                }
+            }
+
+            if (ambiguousCount > 0 && !requiresFile && !requiresRank)
+            {
+                requiresFile = true;
+            }
+        }
+
+        var builder = new StringBuilder();
+        if (pieceChar != '\0')
+        {
+            builder.Append(pieceChar);
+            if (requiresFile)
+            {
+                builder.Append((char)('a' + GetFile(move.From)));
+            }
+            if (requiresRank)
+            {
+                builder.Append((char)('1' + GetRank(move.From)));
+            }
+        }
+        else if (move.IsCapture)
+        {
+            builder.Append((char)('a' + GetFile(move.From)));
+        }
+
+        if (move.IsCapture)
+        {
+            builder.Append('x');
+        }
+
+        AppendSquare(builder, move.To);
+
+        if (move.PromotedPieceType != PromotedPieceType.None)
+        {
+            builder.Append('=');
+            builder.Append(GetPromotionLetter(move.PromotedPieceType));
+        }
+
+        return AppendCheckSuffix(position, move, builder.ToString(), executor);
+    }
+
+    private static string AppendCheckSuffix(Position position, Move move, string san, MoveExecutor executor)
+    {
+        var next = position.Clone();
+        executor.MakeMove(next, move);
+        var isCheck = MoveGeneration.IsSquareAttacked(next,
+            next.WhiteToMove ? next.WhiteKing.GetFirstSquare() : next.BlackKing.GetFirstSquare(),
+            byWhite: !next.WhiteToMove);
+        if (!isCheck)
+        {
+            return san;
+        }
+
+        Span<Move> moves = stackalloc Move[218];
+        var replyCount = MoveGeneration.GenerateLegalMoves(next, moves);
+        return replyCount == 0 ? san + '#' : san + '+';
+    }
+
+    private static void AppendSquare(StringBuilder builder, Square square)
+    {
+        builder.Append((char)('a' + GetFile(square)));
+        builder.Append((char)('1' + GetRank(square)));
+    }
+
+    private static char GetPieceLetter(PieceType pieceType)
+    {
+        return pieceType switch
+        {
+            PieceType.WhiteKnight => 'N',
+            PieceType.WhiteBishop => 'B',
+            PieceType.WhiteRook => 'R',
+            PieceType.WhiteQueen => 'Q',
+            PieceType.WhiteKing => 'K',
+            PieceType.BlackKnight => 'N',
+            PieceType.BlackBishop => 'B',
+            PieceType.BlackRook => 'R',
+            PieceType.BlackQueen => 'Q',
+            PieceType.BlackKing => 'K',
+            _ => '\0'
+        };
+    }
+
+    private static char GetPromotionLetter(PromotedPieceType promotedPieceType)
+    {
+        return promotedPieceType switch
+        {
+            PromotedPieceType.Queen => 'Q',
+            PromotedPieceType.Rook => 'R',
+            PromotedPieceType.Bishop => 'B',
+            PromotedPieceType.Knight => 'N',
+            _ => throw new ArgumentOutOfRangeException(nameof(promotedPieceType), promotedPieceType, "Invalid promotion type.")
+        };
+    }
+
+    private static int GetFile(Square square)
+    {
+        return (int)square % 8;
+    }
+
+    private static int GetRank(Square square)
+    {
+        return (int)square / 8;
+    }
+}
